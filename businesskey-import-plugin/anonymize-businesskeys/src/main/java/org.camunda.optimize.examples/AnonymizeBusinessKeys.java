9ad5a00d00fe062c8d977ed2b11cf61653f74aba
Helene Waechtler <p.h.waechtler@gmail.com>
2020-04-17 16:25:20 +0200

    Implement latest changes to businessKeyPlugin

--- businesskey-import-plugin/anonymize-businesskeys/src/main/java/org.camunda.optimize.examples/AnonymizeBusinessKeys.java
+++ businesskey-import-plugin/anonymize-businesskeys/src/main/java/org.camunda.optimize.examples/AnonymizeBusinessKeys.java
@@ -1,19 +1,10 @@
 package org.camunda.optimize.examples;
 
-import org.camunda.optimize.plugin.importing.businesskey.ProcessInstancePluginDto;
 import org.camunda.optimize.plugin.importing.businesskey.BusinessKeyImportAdapter;
-import java.util.List;
 
 public class AnonymizeBusinessKeys implements BusinessKeyImportAdapter {
 
-  public List<ProcessInstancePluginDto> adaptBusinessKeys(List<ProcessInstancePluginDto> processInstances) {
-    processInstances.stream()
-      .forEach(this::anonymizeBusinessKey);
-    return list;
-  }
-
-  private void anonymizeBusinessKey(ProcessInstancePluginDto processInstance) {
-    final int anonymizedBusinessKey = processInstance.getBusinessKey().hashCode();
-    processInstance.setBusinessKey(String.valueOf(anonymizedBusinessKey));
+  public String adaptBusinessKey(String businessKey) {
+    return String.valueOf(businessKey.hashCode());
   }
 }
